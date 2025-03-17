f7c3eecd1a149c52b2e5dd2d8dfbd8685d946119
Gagan866 <gd0363264@gmail.com>
2025-03-17 06:44:35 +0530

    added new function

--- hello.java
+++ hello.java
@@ -6,9 +6,13 @@ public class hello {
     void morning(){
         System.out.println("morning");
     }
+    void evening(){
+        System.out.println("Evening");
+    }
     public static void main(String[] args) {
         hello h = new hello();
         h.hi();
         h.morning();
+        h.evening();
     }
 }
