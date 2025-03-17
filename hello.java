af018ae0f5cd0d5409fb06fac295a7357c928894
Gagan866 <gd0363264@gmail.com>
2025-03-17 15:49:46 +0530

    updated

--- hello.java
+++ hello.java
@@ -9,10 +9,14 @@ public class hello {
     void evening(){
         System.out.println("Evening");
     }
+    void night(){
+        System.out.println("Night");
+    }
     public static void main(String[] args) {
         hello h = new hello();
         h.hi();
         h.morning();
         h.evening();
+        h.night();
     }
 }
