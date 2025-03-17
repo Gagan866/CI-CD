d616110aed20933ac86427935f9a79027d8b184b
Gagan866 <gd0363264@gmail.com>
2025-03-17 05:55:59 +0530

    java code first

---
+++ hello.java
@@ -0,0 +1,14 @@
+public class hello {
+    void hi() {
+        System.out.println("hi");
+
+    }
+    void morning(){
+        System.out.println("morning");
+    }
+    public static void main(String[] args) {
+        hello h = new hello();
+        h.hi();
+        h.morning();
+    }
+}
